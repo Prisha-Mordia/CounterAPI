3450d11dfdcca8be565eaf4dc71eef77da1ae17b
Punit-Choudhary <itspunitchoudhary@gmail.com>
2024-03-29 21:47:17 +0530

    visitors badge Co-authored-by: Punit Choudhary <itspunitchoudhary@gmail.com> Co-authored-by: Prisha Mordia <prishamordiya@gmail.com>

--- index.js
+++ index.js
@@ -1,58 +1,70 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const Data = require('./models/dataModel');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Data = require("./models/dataModel");
+
 const PORT = process.env.PORT;
+const hostedUrl = process.env.hostedUrl;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    try {
-        return res.sendFile(__dirname + '/index.html');
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message,
-        });
+app.get("/", async (req, res) => {
+  try {
+    const { key } = req.query;
+
+    if (!key) {
+      return res.sendFile(__dirname + "/index.html");
     }
-});
 
-app.get('/:key', async (req, res) => {
-    try {
-        const key = req.params.key;
-        const data = await Data.findOneAndUpdate(
-            { key },
-            { $inc: { value: 1 } },
-            { upsert: true, projection: { _id: 0, key: 1, value: 1 }, new: true }
-        );
+    const data = await Data.findOneAndUpdate(
+      {
+        key,
+      },
+      {
+        $inc: {
+          value: 1,
+        },
+      },
+      {
+        upsert: true,
+        projection: {
+          _id: 0,
+          key: 1,
+          value: 1,
+        },
+        new: true,
+      },
+    );
 
-        return res.status(200).json({
-            key: data.key,
-            value: data.value,
-        });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: error.message,
-        });
-    }
+    return res.render("visitors", {
+      viewCount: data.value,
+      hostedUrl: hostedUrl,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
 app.listen(PORT, () => {
-    try {
-        mongoose.connect(process.env.MONGO_URI)
-            .then(() => {
-                console.log("Connected to MongoDB!");
-            })
-            .catch((error) => {
-                console.error("Error while connecting to MongoDB: " + error.message);
-            })
-        console.log(`PORT WORKING http://localhost:${PORT}`);
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
+  try {
+    mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => {
+        console.log("Connected to MongoDB!");
+      })
+      .catch((error) => {
+        console.error("Error while connecting to MongoDB: " + error.message);
+      });
+    console.log(`PORT WORKING http://localhost:${PORT}`);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 });
